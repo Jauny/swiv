39e6105aa189762fe437021c0197d00d54393b50
Jonathan Pepin <jypepin@gmail.com>
2012-11-14 15:17:09 -0800

    deleted useless moniauth file

--- app/views/layouts/omniauth.rb
+++
@@ -1,7 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "499533736734875", "e866f99a81710e82f5ff921be366508f",
-           :scope => "first_name, last_name, email, user_birthday",
-           :image_size => "square", :display => "popup"
-end
