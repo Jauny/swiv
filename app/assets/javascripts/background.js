9dfafb63e3c45710c950f869d7f2fedfb2f1385d
Jonathan Pepin <jypepin@gmail.com>
2012-11-15 21:40:19 -0800

    added backgrounds

--- app/assets/javascripts/background.js
+++ app/assets/javascripts/background.js
@@ -1,4 +1,6 @@
-var images = ['breakingbad.jpg', 'brothers.jpg', 'gamesofthrone.jpg', 'sonsofanarchy.jpg', 'vampire.jpg'];
+var images = ['breakingbad.jpg', 'breakingbad2.jpg', 'brothers.jpg', 'fringe.jpg', 'gamesofthrone.jpg', 'house.jpg', 
+              'sonsofanarchy.jpg', 'sonsofanarchy2.jpg', 'tbbt.jpg', 'theoffice.jpg', 'trueblood.jpg', 'vampire.jpg',
+              'walkingdead.jpg', 'walkingdead2.jpg'];
 var randomimage = images[Math.floor(Math.random() * images.length)];
 
 $(document).ready(function() {
