4225c25a02bf06c4d650aa3c27017145d3266356
Jonathan Pepin <jypepin@gmail.com>
2012-11-21 11:33:03 -0800

    changed routes to fix signout bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Swiv::Application.routes.draw do
 
-  devise_for :users
+  devise_for :users do
+    get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
 
   root :to => "StaticPages#index"
 
