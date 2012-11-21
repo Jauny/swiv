4346a2b5a503a025af96b14d02b1d6dad11e3333
Jonathan Pepin <jypepin@gmail.com>
2012-11-21 10:06:36 -0800

    added validatable in devise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable, :validatable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
 
   validates :name, presence: true
-  validates :email, presence: true
 
   has_and_belongs_to_many :shows
   has_and_belongs_to_many :episodes
